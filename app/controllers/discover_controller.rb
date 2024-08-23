b74f9a38d7154891a0c5d18d51b7da41a4b36bb7
Lance Nelson <lancen04@gmail.com>
2024-08-23 11:45:44 -0400

    updated test and changed some controller actions

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,17 +1,15 @@
 class DiscoverController < ApplicationController
   def index
-    # binding.pry
-    keyword = params[:search_keywords]
+    # keyword = params[:search_keywords]
     @user = User.find(params[:id])
 
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.params[:api_key] = Rails.application.credentials.tmdb[:key]
-    end
+    # conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+    #   faraday.params[:api_key] = Rails.application.credentials.tmdb[:key]
+    # end
 
-    response = conn.get("/3/search/movie?#{keyword}?&language=en-US&page=1")
+    # response = conn.get("/3/search/movie?#{keyword}?&language=en-US&page=1")
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @movies = json[:results]
-    # binding.pry
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @movies = json[:results]
   end
 end
--- spec/features/movies/search_spec.rb
+++ spec/features/movies/search_spec.rb
@@ -9,12 +9,10 @@ RSpec.describe 'Search by Title', type: :feature do
   it "allows the user to search for top rated movies" do
     VCR.use_cassette("search_movies") do 
       visit "/users/#{@user1.id}/discover"
-      # save_and_open_page
 
       fill_in :search_keywords, with: "Shawshank Redemption"
       click_button 'Search'
 
-      # save_and_open_page
       expect(page.status_code).to eq 200
       
       within(first(".movie")) do
